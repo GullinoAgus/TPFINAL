67aafca32b007942a86bbbda7de253f371ab36ac
agus <agullino@itba.edu.ar>
2020-11-23 17:05:55 -0300

    Muestra

---
+++ header.h
@@ -0,0 +1,8 @@
+//
+// Created by agus on 23/11/20.
+//
+
+#ifndef TPFINAL_HEADER_H
+#define TPFINAL_HEADER_H
+
+#endif //TPFINAL_HEADER_H
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int variabledeestado=0;
+
 int main() {
     printf("Hello, World!\n");
     return 0;
