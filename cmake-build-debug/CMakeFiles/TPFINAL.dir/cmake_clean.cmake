44791f04bff2131373f3aaf7be996e9ab52bdbf3
heladero70 <golinares@itba.edu.ar>
2020-11-24 21:01:56 -0300

    Ya casi anda la lectura de la informacion de los ficheros

---
+++ cmake-build-debug/CMakeFiles/TPFINAL.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TPFINAL.dir/allegroLib.c.o"
+  "CMakeFiles/TPFINAL.dir/data.c.o"
+  "CMakeFiles/TPFINAL.dir/entidad.c.o"
+  "CMakeFiles/TPFINAL.dir/fisica.c.o"
+  "CMakeFiles/TPFINAL.dir/main.c.o"
+  "CMakeFiles/TPFINAL.dir/menu.c.o"
+  "TPFINAL"
+  "TPFINAL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TPFINAL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
