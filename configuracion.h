0b665256de33e7a7e4e226c47cf3b6ee0a4abf1f
agus <agullino@itba.edu.ar>
2020-11-26 00:26:35 -0300

    Agregue texturas

--- configuracion.h
+++ configuracion.h
@@ -7,6 +7,6 @@
 
 #define MODOJUEGO 0     //0 para jugar con la pc, 1 para jugar con la raspi
 #define SCREENWIDHT 1280
-#define SCREENHEIGHT 640
+#define SCREENHEIGHT 720
 
 #endif //TPFINAL_CONFIGURACION_H
