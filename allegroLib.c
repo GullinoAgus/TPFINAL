003be875b1fbf5d79b0647bdbccdd7a6c3032c54
agus <agullino@itba.edu.ar>
2021-01-23 21:20:48 -0300

    arreglito

--- allegroLib.c
+++ allegroLib.c
@@ -42,10 +42,6 @@ int inicializarAllegro(){
         salida = 1;
     }
 
-    if (!al_install_audio()) {          //Inicializo el audio, en caso de error muestro un mensaje
-        salida = 1;
-    }
-
     return salida;
 }
 
@@ -160,7 +156,7 @@ int cargarFuentesMenu(fuente_t **fuente) {
 void destroyResources(bufferRecursos_t *resourcesBuffer){
 
     free(resourcesBuffer->image);
-    free(resourcesBuffer->sound);
+    freeAudio(*resourcesBuffer->sound); //FIXME falta liberar cierta parte del audio, revisar freeAudio
     free(resourcesBuffer->font);
 
 }
