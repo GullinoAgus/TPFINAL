3d373ece79deaa901e84026d8abd7438c25c8c4f
heladero70 <golinares@itba.edu.ar>
2021-01-27 13:10:01 -0300

    test raspi 1

---
+++ cmake-build-debug/CMakeFiles/TPFINAL.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/gonzalo/CLionProjects/TPFINAL/IEvents.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/IEvents.c.o"
+  "/home/gonzalo/CLionProjects/TPFINAL/allegroLib.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/allegroLib.c.o"
+  "/home/gonzalo/CLionProjects/TPFINAL/animacion.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/animacion.c.o"
+  "/home/gonzalo/CLionProjects/TPFINAL/audio.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/audio.c.o"
+  "/home/gonzalo/CLionProjects/TPFINAL/data.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/data.c.o"
+  "/home/gonzalo/CLionProjects/TPFINAL/entidades.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/entidades.c.o"
+  "/home/gonzalo/CLionProjects/TPFINAL/fisica.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/fisica.c.o"
+  "/home/gonzalo/CLionProjects/TPFINAL/gamelogic.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/gamelogic.c.o"
+  "/home/gonzalo/CLionProjects/TPFINAL/level.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/level.c.o"
+  "/home/gonzalo/CLionProjects/TPFINAL/main.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/main.c.o"
+  "/home/gonzalo/CLionProjects/TPFINAL/menu.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/menu.c.o"
+  "/home/gonzalo/CLionProjects/TPFINAL/render.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/render.c.o"
+  "/home/gonzalo/CLionProjects/TPFINAL/times.c" "/home/gonzalo/CLionProjects/TPFINAL/cmake-build-debug/CMakeFiles/TPFINAL.dir/times.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TPFINAL.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TPFINAL.dir/IEvents.c.o"
+  "CMakeFiles/TPFINAL.dir/allegroLib.c.o"
+  "CMakeFiles/TPFINAL.dir/animacion.c.o"
+  "CMakeFiles/TPFINAL.dir/audio.c.o"
+  "CMakeFiles/TPFINAL.dir/data.c.o"
+  "CMakeFiles/TPFINAL.dir/entidades.c.o"
+  "CMakeFiles/TPFINAL.dir/fisica.c.o"
+  "CMakeFiles/TPFINAL.dir/gamelogic.c.o"
+  "CMakeFiles/TPFINAL.dir/level.c.o"
+  "CMakeFiles/TPFINAL.dir/main.c.o"
+  "CMakeFiles/TPFINAL.dir/menu.c.o"
+  "CMakeFiles/TPFINAL.dir/render.c.o"
+  "CMakeFiles/TPFINAL.dir/times.c.o"
+  "TPFINAL"
+  "TPFINAL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TPFINAL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
