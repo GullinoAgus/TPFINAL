d90bdbf14a7d64ba136503e72c306c2a2bd1b408
agus <agullino@itba.edu.ar>
2021-01-29 14:53:15 -0300

    Ruiditos y mas ;)

--- fisica.h
+++ fisica.h
@@ -1,16 +1,22 @@
-//
-// Created by agus on 23/11/20.
-//
-
+/***************************************************************************//**
+  @file     +fisica.h+
+  @brief    +Libreria para motor de fisica+
+  @author   +Grupo 1+
+ ******************************************************************************/
 #ifndef TPFINAL_FISICA_H
 #define TPFINAL_FISICA_H
 
-
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
 #define VELOCIDADXMAX (0.3f)
 #define VELOCIDADYMAX (0.6f)
 #define GRAVEDAD 0.009f
 #define INERCIA 0.975f
 
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
 typedef struct {
     float posx;
     float posy;
@@ -20,7 +26,9 @@ typedef struct {
     float vely;
 } fisica_t;
 
-
+/*******************************************************************************
+ * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
 void* fisica(void* entrada);
 void movePlayer(int direction, void* player);
 
